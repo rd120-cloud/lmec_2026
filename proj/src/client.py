549c8a81b84d9c5bd0e61f2a5be3769e8ff748ee
Reuben Danyali <you@example.com>
2026-07-29 04:22:53 -0400

    Client scripts to download from allmaps and check manifests and whatnot

--- proj/src/client.py
+++ proj/src/client.py
@@ -1,3 +1,12 @@
-# Based on atlascopify.py by Ian Spangler
-# Accessible at https://github.com/bplmaps/atlascope-utilities/tree/master/modern-workflow
-# Updated for specific use with this project, and for more general IIIF database workflow development
+import requests
+from .cli import verbose_option
+
+@verbose_option
+def download_images(identifier):
+    """
+    Downloads images from IIIF given the manifest UID and saves them to the specified output path.
+
+    Args:
+        identifier (str): The manifest UID of the image collection to download.
+    """
+    allmapsManifest = requests.get(f'https://annotations.allmaps.org/?url=https://www.digitalcommonwealth.org/search/{identifier}/manifest.json').json()
